217830f9dcdb60c1b2f2ce44a23bd4e53db6255f
wmvieira21 <90009567+wmvieira21@users.noreply.github.com>
2023-05-01 21:01:57 -0200

    hp

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let login = document.querySelector('.main-navbar__login--button');
+let menuNavBar = document.querySelector('.main-navbar__menu--button');
+let menuSideNavBar = document.querySelector('.sidenav-navbar__menu--button');
+let sideNavBar = document.querySelector('.sidenav-sidebar');
+
 
 login.addEventListener('click', () => {
-    alert('Not implemented');
+    alert('Action is not available!');
+});
+
+menuNavBar.addEventListener('click', () => {
+    sideNavBar.classList.remove('close-sidenav');
+});
+
+menuSideNavBar.addEventListener('click', () => {
+    sideNavBar.classList.add('close-sidenav');
 });
