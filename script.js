2f8e97895f0342c32bb3c551c47045f4bce34bae
wmvieira-philips <william.vieira@philips.com>
2023-05-02 12:38:13 -0300

    hp

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ let menuNavBar = document.querySelector('.main-navbar__menu--button');
 let menuSideNavBar = document.querySelector('.sidenav-navbar__menu--button');
 let sideNavBar = document.querySelector('.sidenav-sidebar');
 
-
-login.addEventListener('click', () => {
-    alert('Action is not available!');
-});
-
 menuNavBar.addEventListener('click', () => {
     sideNavBar.classList.remove('close-sidenav');
+    setTimeout(() => {
+        sideNavBar.classList.add('sidenav-sidebar__transition');
+    }, 10);
 });
 
 menuSideNavBar.addEventListener('click', () => {
+    sideNavBar.classList.remove('sidenav-sidebar__transition');
     sideNavBar.classList.add('close-sidenav');
 });
